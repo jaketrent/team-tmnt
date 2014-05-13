32f032bd45dc2fbe13278421f8ada463aa767470
jaketrent <trent.jake@gmail.com>
2014-05-12 20:36:36 -0600

    add service test

--- test/spec/services/turtle-service.spec.js
+++ test/spec/services/turtle-service.spec.js
@@ -11,8 +11,25 @@ describe('Service: TurtleService', function () {
     TurtleService = _TurtleService_;
   }));
 
-  it('should do something', function () {
-    expect(!!TurtleService).toBe(true);
-  });
+  it('should do get a team', inject(function ($httpBackend) {
+    var teamId = 1;
+    $httpBackend.expectGET('http://pure-ocean-3603.herokuapp.com/team/' + teamId).respond({id: 1, name: 'Streganona'});
 
+    TurtleService.getTeam(1).then(function (result) {
+      expect(result.name).toBe('Streganona');
+    });
+    $httpBackend.flush();
+
+  }));
+
+  it('should do get teams', inject(function ($httpBackend) {
+    $httpBackend.expectGET('http://pure-ocean-3603.herokuapp.com/team').respond([{id: 1, name: 'Streganona'}, {id: 2, name: 'Mary Poppins'}]);
+
+    TurtleService.getTeams().then(function (result) {
+      expect(result[0].name).toBe('Streganona');
+      expect(result[1].name).toBe('Mary Poppins');
+    });
+    $httpBackend.flush();
+
+  }));
 });
